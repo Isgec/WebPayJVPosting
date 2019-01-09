17c49720991106215dc649c36f1f6910fca36159
0340LalitGupta <lalit@isgec.co.in>
2019-01-09 14:58:49 +0530

    Initial commit

---
+++ WebPayJVPosting/JVPostingMain.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JVPostingMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.txtJVFile = New System.Windows.Forms.TextBox()
+    Me.cmdBrowseJVTextFile = New System.Windows.Forms.Button()
+    Me.ofd1 = New System.Windows.Forms.OpenFileDialog()
+    Me.cmdConvertPSV = New System.Windows.Forms.Button()
+    Me.Button1 = New System.Windows.Forms.Button()
+    Me.pBar = New System.Windows.Forms.ProgressBar()
+    Me.Label2 = New System.Windows.Forms.Label()
+    Me.pLbl = New System.Windows.Forms.Label()
+    Me.chkSplit = New System.Windows.Forms.CheckBox()
+    Me.SuspendLayout()
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = True
+    Me.Label1.Location = New System.Drawing.Point(13, 13)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(109, 13)
+    Me.Label1.TabIndex = 0
+    Me.Label1.Text = "WebPay JV Text File:"
+    '
+    'txtJVFile
+    '
+    Me.txtJVFile.Location = New System.Drawing.Point(128, 6)
+    Me.txtJVFile.Name = "txtJVFile"
+    Me.txtJVFile.Size = New System.Drawing.Size(356, 20)
+    Me.txtJVFile.TabIndex = 1
+    '
+    'cmdBrowseJVTextFile
+    '
+    Me.cmdBrowseJVTextFile.Location = New System.Drawing.Point(501, 6)
+    Me.cmdBrowseJVTextFile.Name = "cmdBrowseJVTextFile"
+    Me.cmdBrowseJVTextFile.Size = New System.Drawing.Size(32, 19)
+    Me.cmdBrowseJVTextFile.TabIndex = 2
+    Me.cmdBrowseJVTextFile.Text = ". . ."
+    Me.cmdBrowseJVTextFile.UseVisualStyleBackColor = True
+    '
+    'ofd1
+    '
+    Me.ofd1.DefaultExt = "txt"
+    Me.ofd1.FileName = "OpenFileDialog1"
+    Me.ofd1.Title = "Select WebPay JV Text File"
+    '
+    'cmdConvertPSV
+    '
+    Me.cmdConvertPSV.Location = New System.Drawing.Point(194, 32)
+    Me.cmdConvertPSV.Name = "cmdConvertPSV"
+    Me.cmdConvertPSV.Size = New System.Drawing.Size(128, 23)
+    Me.cmdConvertPSV.TabIndex = 3
+    Me.cmdConvertPSV.Text = "Convert to PSV"
+    Me.cmdConvertPSV.UseVisualStyleBackColor = True
+    '
+    'Button1
+    '
+    Me.Button1.Location = New System.Drawing.Point(334, 32)
+    Me.Button1.Name = "Button1"
+    Me.Button1.Size = New System.Drawing.Size(128, 23)
+    Me.Button1.TabIndex = 4
+    Me.Button1.Text = "Convert to XML"
+    Me.Button1.UseVisualStyleBackColor = True
+    '
+    'pBar
+    '
+    Me.pBar.Location = New System.Drawing.Point(128, 62)
+    Me.pBar.Name = "pBar"
+    Me.pBar.Size = New System.Drawing.Size(356, 23)
+    Me.pBar.Step = 1
+    Me.pBar.TabIndex = 5
+    '
+    'Label2
+    '
+    Me.Label2.AutoSize = True
+    Me.Label2.Location = New System.Drawing.Point(10, 67)
+    Me.Label2.Name = "Label2"
+    Me.Label2.Size = New System.Drawing.Size(85, 13)
+    Me.Label2.TabIndex = 6
+    Me.Label2.Text = "XML Conversion"
+    '
+    'pLbl
+    '
+    Me.pLbl.AutoSize = True
+    Me.pLbl.Location = New System.Drawing.Point(498, 67)
+    Me.pLbl.Name = "pLbl"
+    Me.pLbl.Size = New System.Drawing.Size(13, 13)
+    Me.pLbl.TabIndex = 7
+    Me.pLbl.Text = "0"
+    '
+    'chkSplit
+    '
+    Me.chkSplit.AutoSize = True
+    Me.chkSplit.Checked = True
+    Me.chkSplit.CheckState = System.Windows.Forms.CheckState.Checked
+    Me.chkSplit.Location = New System.Drawing.Point(68, 36)
+    Me.chkSplit.Name = "chkSplit"
+    Me.chkSplit.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+    Me.chkSplit.Size = New System.Drawing.Size(75, 17)
+    Me.chkSplit.TabIndex = 8
+    Me.chkSplit.Text = "Split in IJT"
+    Me.chkSplit.UseVisualStyleBackColor = True
+    '
+    'JVPostingMain
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(562, 326)
+    Me.Controls.Add(Me.chkSplit)
+    Me.Controls.Add(Me.pLbl)
+    Me.Controls.Add(Me.Label2)
+    Me.Controls.Add(Me.pBar)
+    Me.Controls.Add(Me.Button1)
+    Me.Controls.Add(Me.cmdConvertPSV)
+    Me.Controls.Add(Me.cmdBrowseJVTextFile)
+    Me.Controls.Add(Me.txtJVFile)
+    Me.Controls.Add(Me.Label1)
+    Me.Name = "JVPostingMain"
+    Me.Text = "JVPostingMain"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents Label1 As System.Windows.Forms.Label
+  Friend WithEvents txtJVFile As System.Windows.Forms.TextBox
+  Friend WithEvents cmdBrowseJVTextFile As System.Windows.Forms.Button
+  Friend WithEvents ofd1 As System.Windows.Forms.OpenFileDialog
+  Friend WithEvents cmdConvertPSV As System.Windows.Forms.Button
+  Friend WithEvents Button1 As System.Windows.Forms.Button
+  Friend WithEvents pBar As System.Windows.Forms.ProgressBar
+  Friend WithEvents Label2 As System.Windows.Forms.Label
+  Friend WithEvents pLbl As System.Windows.Forms.Label
+  Friend WithEvents chkSplit As System.Windows.Forms.CheckBox
+End Class
---
+++ WebPayJVPosting/JVPostingMain.vb
@@ -0,0 +1,308 @@
+﻿Imports System.Text
+Imports System.Xml
+Public Class JVPostingMain
+
+  Private Sub cmdBrowseJVTextFile_Click(sender As System.Object, e As System.EventArgs) Handles cmdBrowseJVTextFile.Click
+    If txtJVFile.Text <> String.Empty Then
+      ofd1.InitialDirectory = IO.Path.GetFullPath(txtJVFile.Text)
+    End If
+    ofd1.ShowDialog()
+    If ofd1.FileName <> String.Empty Then
+      txtJVFile.Text = ofd1.FileName
+    End If
+  End Sub
+
+  Private Sub cmdConvertPSV_Click(sender As System.Object, e As System.EventArgs) Handles cmdConvertPSV.Click
+    If txtJVFile.Text = String.Empty Then
+      MsgBox("No file specified.")
+      Exit Sub
+    End If
+    Dim tr As IO.StreamReader = New IO.StreamReader(txtJVFile.Text)
+    Dim tw As IO.StreamWriter = New IO.StreamWriter(IO.Path.GetDirectoryName(txtJVFile.Text) & "\" & IO.Path.GetFileNameWithoutExtension(txtJVFile.Text) & ".psv")
+    Dim str1 As String = tr.ReadLine
+    Do While str1 IsNot Nothing
+
+      str1 = str1.Trim
+      If str1.Length >= 106 Then
+        str1 = str1.Insert(101, "|")
+      End If
+      str1 = str1.Insert(78, "|")
+      str1 = str1.Insert(48, "|")
+      str1 = str1.Insert(47, "|")
+      str1 = str1.Insert(26, "|")
+      str1 = str1.Insert(21, "|")
+      str1 = str1.Insert(15, "|")
+      str1 = str1.Insert(3, "|")
+      tw.WriteLine(str1)
+      str1 = tr.ReadLine
+    Loop
+    tr.Close()
+    tw.Close()
+  End Sub
+
+  Delegate Sub tmp(ByVal jvFile As String, ByVal bar As ProgressBar, ByVal lbl As Label, ByVal splitit As Boolean)
+  Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+    If txtJVFile.Text = String.Empty Then
+      MsgBox("No file specified.")
+      Exit Sub
+    End If
+    Dim ds As tmp = AddressOf ConvertData
+    ds.BeginInvoke(txtJVFile.Text, pBar, pLbl, chkSplit.Checked, Nothing, Nothing)
+  End Sub
+  Private Sub ConvertData(ByVal jvFile As String, ByVal bar As ProgressBar, ByVal lbl As Label, ByVal splitit As Boolean)
+    Dim oBj As ConvertSalaryData = New ConvertSalaryData(jvFile, pBar, pLbl, splitit)
+    oBj.ConvertSalaryData()
+  End Sub
+End Class
+
+
+Public Class ConvertSalaryData
+  Sub New(ByVal jvFile As String, ByVal bar As ProgressBar, ByVal lbl As Label, ByVal SplitIt As Boolean)
+    txtJVFile = jvFile
+    pBar = bar
+    pLbl = lbl
+    chksplit = SplitIt
+  End Sub
+  Delegate Sub tmp1(ByVal str As String)
+  Dim pBar As ProgressBar = Nothing
+  Dim pLbl As Label = Nothing
+  Dim txtJVFile As String = ""
+  Dim chksplit As Boolean = False
+  Public Sub ConvertSalaryData()
+    Dim CardNo As String = "340"
+    Dim psvFile As String = IO.Path.GetDirectoryName(txtJVFile) & "\" & IO.Path.GetFileNameWithoutExtension(txtJVFile) & ".psv"
+
+    Dim oVchs As New List(Of Vouchers)
+    Dim FileNo As Integer = 0
+    Dim SplitXML As Boolean = False
+    Dim SplitStart As Integer = 0
+    Dim SplitEnd As Integer = 2000
+    Dim srno As Integer = 0
+    Dim TotLine As Integer = 0
+    Dim pb As Integer = 0
+    Dim pv As Integer = 0
+    Dim aCmp() As String = Nothing
+    Dim rstr() As String = Nothing
+    Dim oTS As IO.StreamReader = New IO.StreamReader(psvFile)
+    Dim tmp As String = oTS.ReadLine
+    Do While tmp IsNot Nothing
+      curVal(TotLine)
+      TotLine += 1
+      rstr = tmp.Split("|".ToCharArray)
+      Try
+        Dim Found As Boolean = False
+        If aCmp Is Nothing Then
+          ReDim aCmp(0)
+          aCmp(0) = rstr(0)
+        End If
+        For Each ttmp As String In aCmp
+          If ttmp.ToLower = rstr(0).ToLower Then
+            Found = True
+            Exit For
+          End If
+        Next
+        If Not Found Then
+          ReDim Preserve aCmp(aCmp.Length)
+          aCmp(aCmp.Length - 1) = rstr(0)
+        End If
+      Catch ex As Exception
+        curVal(TotLine & " : " & ex.Message)
+        Threading.Thread.Sleep(12000)
+      End Try
+      tmp = oTS.ReadLine
+    Loop
+    oTS.Close()
+    InitBar(TotLine)
+    pv = TotLine
+    For Each cmp As String In aCmp
+      If cmp.ToLower = "ijt" Then
+        If chksplit Then
+          SplitXML = True
+        Else
+          SplitXML = False
+        End If
+      End If
+      SplitStart = 0
+      SplitEnd = 2000
+      FileNo = 0
+      'To execute twice split file below label is used
+ExecuteSplit:
+      pv = TotLine
+      pb = 0
+      srno = 0
+      oVchs = New List(Of Vouchers)
+      FileNo += 1
+      oTS = New IO.StreamReader(psvFile)
+      Dim rrStr As String = oTS.ReadLine
+      Do While rrStr IsNot Nothing
+        Try
+          pb += 1
+          pv -= 1
+          curBar(pb)
+          curVal(pv)
+          rstr = rrStr.Split("|".ToCharArray)
+          If rstr(0).ToLower <> cmp.ToLower Then
+            rrStr = oTS.ReadLine
+            Continue Do
+          End If
+          If SplitXML Then
+            If Convert.ToInt32(rstr(3).Trim) < SplitStart Or Convert.ToInt32(rstr(3).Trim) > SplitEnd Then
+              rrStr = oTS.ReadLine
+              Continue Do
+            End If
+          End If
+          Select Case rstr(0).ToUpper
+            Case "IJT"
+              rstr(0) = 200
+            Case "HOC"
+              rstr(0) = 400
+            Case "RND"
+              rstr(0) = 290
+          End Select
+          Dim oVch As New Vouchers
+          srno += 1
+          With oVch
+            .BatchCompany = rstr(0)
+            .TargetCompany = rstr(0)
+            .ForSoftware = "10"
+            rstr(7) = rstr(7).Trim
+            .VoucherDate = rstr(7).Substring(6, 2) & "/" & rstr(7).Substring(4, 2) & "/" & rstr(7).Substring(0, 4)
+            .ProcessID = ""
+            .SerialNo = srno
+            .BusinessPartner = ""
+            .TranType = "JVN"
+            .Series = ""
+            .LedgerAc = Convert.ToInt64(rstr(1))
+            .Dim1 = ""
+            .Dim2 = rstr(2).ToString.ToUpper.Trim.Replace("&", "&amp;")
+            .Dim3 = Convert.ToInt32(rstr(3))
+            Try
+              .Dim4 = rstr(8).Trim
+            Catch ex As Exception
+            End Try
+            .Dim5 = "UP"
+            .Currency = "INR"
+            .Amount = rstr(4)
+            .DrCr = IIf(rstr(5) = "C", 2, 1)
+            .Remarks = rstr(6).Replace(",", " ")
+            .CreatedBy = CardNo
+            .BatchNo = ""
+            .DocumentNo = ""
+            .LineNo = ""
+            .DocumentID = ""
+          End With
+          oVchs.Add(oVch)
+          rrStr = oTS.ReadLine
+        Catch ex As Exception
+        End Try
+      Loop
+      oTS.Close()
+      If oVchs.Count > 0 Then
+        WriteSalaryXML(oVchs, FileNo, psvFile, cmp)
+      End If
+      If SplitXML Then
+        SplitStart = SplitEnd + 1
+        SplitEnd += 7999
+        If SplitStart > TotLine Then
+          Continue For
+        End If
+        GoTo ExecuteSplit
+      End If
+    Next
+done:
+    curVal("DONE")
+    pBar = Nothing
+    pLbl = Nothing
+  End Sub
+  Private Sub WriteSalaryXML(ByVal oVchs As List(Of Vouchers), ByVal FileNo As String, ByVal pName As String, ByVal cmp As String)
+    Dim FName As String = IO.Path.GetFileNameWithoutExtension(pName)
+    Dim FPath As String = IO.Path.GetDirectoryName(pName)
+    Dim mFileName As String = "Salary_" & FileNo & "_" & cmp & "_" & FName.ToUpper & ".xml"
+    Dim oTW As IO.StreamWriter = New IO.StreamWriter(FPath & "/" & mFileName)
+    oTW.WriteLine("<?xml version=""1.0"" encoding=""utf-8""?>")
+
+    oTW.WriteLine("<Companies>")
+    oTW.WriteLine("  <Company name=""" & oVchs(0).BatchCompany & """>")
+    oTW.WriteLine("  <Lines>")
+    For Each oVch As Vouchers In oVchs
+      oTW.WriteLine("      <Line PrkLedgerID=""" & oVch.DocumentID & """  ProcessID="""" SerialNo="""" BatchNo="""" DocumentNo="""" LineNo="""" GetBatchDocument=""" & oVch.BatchCompany & "," & oVch.ForSoftware & "," & oVch.VoucherDate & "," & "[ProcessID]" & "," & "[SerialNo]" & """>" & oVch.BatchCompany & "," & oVch.TargetCompany & "," & oVch.ForSoftware & "," & oVch.VoucherDate & "," & "[ProcessID]" & "," & oVch.SerialNo & "," & oVch.BusinessPartner & "," & oVch.TranType & "," & oVch.Series & "," & oVch.LedgerAc & "," & oVch.Dim1 & "," & oVch.Dim2 & "," & oVch.Dim3 & "," & oVch.Dim4 & "," & oVch.Dim5 & "," & oVch.Currency & "," & oVch.Amount & "," & oVch.DrCr & "," & oVch.Remarks & "," & oVch.CreatedBy & "," & oVch.CF & "</Line>")
+    Next
+    oTW.WriteLine("    </Lines>")
+    oTW.WriteLine("    <Batch>" & oVchs(0).BatchCompany & "," & oVchs(0).ForSoftware & "," & oVchs(0).VoucherDate & "," & "[ProcessID]" & "," & oVchs(0).TranType & "</Batch>")
+    oTW.WriteLine("    <Errors><Error></Error></Errors>")
+    oTW.WriteLine("  </Company>")
+    oTW.WriteLine("</Companies>")
+    oTW.Close()
+    Dim tr As IO.StreamReader = New IO.StreamReader(FPath & "/" & mFileName)
+    Dim sb As StringBuilder = New StringBuilder
+    Dim ch As Char
+    Dim str As String = tr.ReadToEnd
+    tr.Close()
+    For Each ch In str
+      If (XmlConvert.IsXmlChar(ch)) Then
+        sb.Append(ch)
+      End If
+    Next
+    oTW = New IO.StreamWriter(FPath & "/" & mFileName)
+    oTW.Write(sb.ToString)
+    oTW.Close()
+
+  End Sub
+
+  Private Class Vouchers
+    Public BatchCompany As String
+    Public ForSoftware As String
+    Public ProcessID As String  ' Unique Number will be returned from BaaN, to use in subsiquent line
+    Public VoucherDate As String
+    Public SerialNo As String ' Will be returned from BaaN
+    Public BusinessPartner As String
+    Public LedgerAc As String
+    Public Dim1 As String
+    Public Dim2 As String
+    Public Dim3 As String
+    Public Dim4 As String
+    Public Dim5 As String
+    Public Currency As String 'INR
+    Public Amount As String
+    Public DrCr As String '1-Debit, 2-Credit
+    Public TargetCompany As String 'Employees Company in case of Cash Vch
+    Public Remarks As String '32 Chars
+    Public TranType As String 'JVR,CSH,CS1, IMprest
+    Public Series As String 'Blank to use default series from BaaN else specify to use series
+    Public CreatedBy As String 'LoginID of voucher posting user
+    Public BatchNo As String 'Will be returned from BaaN
+    Public DocumentNo As String 'Will be returned from BaaN
+    Public LineNo As String 'Will be returned from BaaN
+    Public CF As Decimal = 1.0 'Conversion Factor
+
+    'Added Fields
+    Public DocumentID As String
+
+  End Class
+  Private Sub curVal(ByVal str As String)
+    If pLbl.InvokeRequired Then
+      pLbl.Invoke(New tmp1(AddressOf curVal), str)
+    Else
+      pLbl.Text = str
+    End If
+  End Sub
+  Private Sub curBar(ByVal str As String)
+    If pBar.InvokeRequired Then
+      pBar.Invoke(New tmp1(AddressOf curBar), str)
+    Else
+      Try
+        pBar.Value = str
+      Catch ex As Exception
+      End Try
+    End If
+  End Sub
+  Private Sub InitBar(ByVal str As String)
+    If pBar.InvokeRequired Then
+      pBar.Invoke(New tmp1(AddressOf InitBar), str)
+    Else
+      pBar.Maximum = str
+      pBar.Value = 0
+    End If
+  End Sub
+
+End Class
